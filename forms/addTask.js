d6cf5b48ad5da4d2f5c13be9e27b315a46d5f9f2
Stifec <stif.1974@gmail.com>
2024-11-09 17:44:10 +0200

    FIX database (delete note)

--- forms/addTask.js
+++ forms/addTask.js
@@ -1,17 +1,14 @@
 
-
 //===== create
 let db;
+const openOrCreateDB = window.indexedDB.open('todo_db', 1);
 
-    const openOrCreateDB = window.indexedDB.open('todo_db', 1);
+openOrCreateDB.addEventListener('error', () => console.error('Error opening DB'));
 
-    openOrCreateDB.addEventListener('error', () => console.error('Error opening DB'));
-
-    openOrCreateDB.addEventListener('success', () => {
-        console.log('Successfully opened DB');
-        db = openOrCreateDB.result;
-
-    });
+openOrCreateDB.addEventListener('success', () => {
+    console.log('Successfully opened DB');
+    db = openOrCreateDB.result;
+});
 
 openOrCreateDB.addEventListener('upgradeneeded', init => {
     db = init.target.result;
@@ -55,7 +52,51 @@ function addTodo(e) {
 }
 
 
+//===== show
 
+function showTodos() {
+    while (todos.firstChild) {
+        todos.removeChild(todos.firstChild);
+    }
+    const objectStore = db.transaction('note_tb').objectStore('note_tb');              // note
+    objectStore.openCursor().addEventListener('success', e => {
 
+        const pointer = e.target.result;
+        console.log('>>>>', pointer);
+
+        if(pointer) {
+            const listItem = document.createElement('div');
+            const h3 = document.createElement('h3');
+            const pg = document.createElement('p');
+            listItem.appendChild(h3);
+            listItem.appendChild(pg);
+            todos.appendChild(listItem);
+            h3.textContent = pointer.value.title;
+            pg.textContent = pointer.value.body;
+            listItem.setAttribute('data-id', pointer.value.id);
+            const deleteBtn = document.createElement('button');
+            listItem.appendChild(deleteBtn);
+            deleteBtn.textContent = 'Remove';
+            deleteBtn.addEventListener('click', deleteItem);
+            pointer.continue();
+        } else {
+            if(!todos.firstChild) {
+                const listItem = document.createElement('div');
+                listItem.textContent = 'No Todo.'
+                todos.appendChild(listItem);
+            }
+
+            console.log('Todos all shown');
+        }
+    });
+}
+
+//==============
+
+// openOrCreateDB.addEventListener('success', () => {
+//     console.log('Successfully opened DB');
+//     db = openOrCreateDB.result;
+//     showTodos();
+// });
 
 
--- notes.js
+++ notes.js
@@ -1,10 +1,12 @@
 
 // ============== Open database ============
-openData(readNote)
+
 let db;
+let openOrCreateDB;
+openData(readNote)
 function openData(dat) {
 
-    const openOrCreateDB = window.indexedDB.open('todo_db', 1);
+    openOrCreateDB = window.indexedDB.open('todo_db', 1);
 
     openOrCreateDB.onsuccess = function (e) {
         db = e.target.result;
@@ -13,8 +15,10 @@ function openData(dat) {
     }
   }
 
+
+
 // =========== READ database =================
-const todos = document.querySelector('ol');
+const todos = document.querySelector('.list');
 function readNote () {
 
     const  transaction = db.transaction(["note_tb"]);
@@ -25,31 +29,92 @@ function readNote () {
         const cursor = e.target.result;
 
         if (cursor) {
-            const listItem = document.createElement('li');
-            const h3 = document.createElement('h3');
-            const pg = document.createElement('p');
-            listItem.appendChild(h3);
-            listItem.appendChild(pg);
-            todos.appendChild(listItem);
-            h3.textContent = cursor.value.title;
-            pg.textContent = cursor.value.body;
-            listItem.setAttribute('data-id', cursor.value.id);
-            const deleteBtn = document.createElement('button');
-            listItem.appendChild(deleteBtn);
-            deleteBtn.textContent = 'Remove';
-            // deleteBtn.addEventListener('click', deleteItem);
 
-            console.log(`ID: ${cursor.key}, Value: ${cursor.value.title}, Value2: ${cursor.value.content}`);
+            let newNote = document.createElement("div");
+            newNote.classList.add("item-note");
+            newNote.setAttribute('data-id', cursor.value.id)
+
+            // newNote.innerHTML = `
+            //     <h3>${cursor.value.title}</h3>
+            //     <p class="item-discription">${cursor.value.body}</p>
+            //     <div class="btn-note">
+            //        <button>
+            //             <svg class="btn-note-edit"><use href="../images/1symbol-defs.svg#icon-pencil">pencil</use></svg>
+            //             Edit
+            //         </button>
+            //         <button class="delete" id="${cursor.value.id}">
+            //             <svg class="btn-note-edit"><use href="../images/1symbol-defs.svg#icon-bin2">bin2</use></svg>
+            //             Delete ${cursor.value.id}
+            //         </button>
+            //     </div>
+            // `;
+            const h3 =document.createElement('h3')
+            const pg = document.createElement('p')
+            pg.classList.add("item-discription")
+            pg.textContent = cursor.value.body
+            h3.textContent = cursor.value.title
+            newNote.appendChild(h3);
+            newNote.appendChild(pg);
+            const btnGroup = document.createElement('div')
+            btnGroup.classList.add('btn-note')
+            newNote.appendChild(btnGroup)
+            const btn = document.createElement('button')
+            btn.classList.add('delete')
+            btn.textContent = 'DELETE'
+            btnGroup.appendChild(btn)
+            btn.addEventListener('click', deleteItem)
+
+
+            todos.appendChild(newNote);
+            // let deleteBtn = document.querySelector('.delete')
+            // console.log('>>>', deleteBtn)
+            // deleteBtn.addEventListener('click', deleteItem)
             cursor.continue();
+
+
+
         } else {
             if(!todos.firstChild) {
-                const listItem = document.createElement('li');
-                listItem.textContent = 'No Todo.'
-                todos.appendChild(listItem);
+                let newNote = document.createElement('div');
+                newNote.classList.add("item-note");
+                newNote.innerHTML = `
+                <h3>No todos.</h3>
+                `;
+                todos.appendChild(newNote);
             }
             console.log("All read notes")
         }
 
+
     }
 }
+
+//====== delete
+
+function deleteItem(e) {
+    console.log("DELETE !!!!")
+    const todoId = Number(e.target.parentNode.parentNode.getAttribute('data-id'));
+
+    console.log('id>>>', todoId)
+
+    const transaction = db.transaction(["note_tb"], "readwrite");
+    const objectStore = transaction.objectStore("note_tb");
+    objectStore.delete(todoId);
+    transaction.addEventListener('complete', () => {
+        e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+        alert(`Note with id of ${todoId} deleted`)
+        console.log(`Note:${todoId} deleted.`);
+        if(!todos.firstChild) {
+            let newNote = document.createElement('div');
+            newNote.classList.add("item-note");
+            newNote.innerHTML = `
+                <h3>No todos.</h3>
+                `;
+            todos.appendChild(newNote);
+        }
+    });
+    transaction.addEventListener('error', () => console.log('Transaction error'));
+}
+
+
 
